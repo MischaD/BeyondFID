43d383992d125b5f76240bc59a48a00f8935bf2e
Mischa <44726551+MischaD@users.noreply.github.com>
2025-10-20 14:20:55 +0200

    Refactor data retrieval functions in __init__.py
    Refactor get_data_csv to use 'FileName' directly and add get_data_from_list function.

--- beyondfid/data/__init__.py
+++ beyondfid/data/__init__.py
@@ -15,10 +15,9 @@ def hash_dataset_path(dataset_root_dir, img_list, descriptor=""):
 
 def get_data_csv(path, fe_name, config=None, split=None):
     data_csv = pd.read_csv(path)
-    if split is not None:
+    if split is not None: 
         data_csv = data_csv[data_csv["Split"]==split]
-    filename_key = config.get("filename_key", "FileName") if config else "FileName"
-    file_list = list(data_csv[filename_key])
+    file_list = list(data_csv["FileName"])
     output_filename = hash_dataset_path(os.path.dirname(path),img_list=file_list, descriptor=fe_name)
     return file_list, os.path.basename(output_filename)
 
@@ -65,6 +64,13 @@ def get_data_from_folder(path, fe_name, config=None):
     logger.info(f"{len(file_list)} files found for path {path}")
     return file_list, os.path.basename(output_filename)
 
+def get_data_from_list(file_list, fe_name): 
+    file_list = sorted(file_list)
+    logger.info(f"{len(file_list)} files found in list")
+    output_filename = hash_dataset_path(os.path.dirname(path),img_list=file_list, descriptor=fe_name)
+    return file_list, output_filename
+ 
+
 
 def get_data(config, path, fe_name, split):
     """Returns list of files in path. Path can be csv or folder that will be searched recursively. 
@@ -74,6 +80,9 @@ def get_data(config, path, fe_name, split):
         # path is dict containing a list of images already loaded. 
         file_key, img_list = next(iter(path.items()))
         return img_list, "hashdata_" + fe_name + "_" + f"{file_key}" 
+
+    elif isinstance(path, list): 
+        return get_data_from_list(path, fe_name)
 
     if path.endswith(".csv"):
         return get_data_csv(path, fe_name, config, split)
